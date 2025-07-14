3ff8837d0ab21c08cd77ec882a0c79084252682b
ddlcaleb <caleb@digitaldiligence.co.nz>
2025-07-14 09:59:02 +0800

    Update nztaicons.js

--- nztaicons.js
+++ nztaicons.js
@@ -1,18 +1,18 @@
 const nztaIcons = {
   "Road Closure": L.icon({
-    iconUrl: './icons/road-closures.svg',
+    iconUrl: 'icons/road-closures.svg',
     iconSize: [30, 30]
   }),
   "Road Hazard": L.icon({
-    iconUrl: './icons/road-hazards.svg',
+    iconUrl: 'icons/road-hazards.svg',
     iconSize: [30, 30]
   }),
   "Area Warning": L.icon({
-    iconUrl: './icons/area-warnings.svg',
+    iconUrl: 'icons/area-warnings.svg',
     iconSize: [30, 30]
   }),
   "Road Work": L.icon({
-    iconUrl: './icons/road-works.svg',
+    iconUrl: 'icons/road-works.svg',
     iconSize: [30, 30]
   }),
   "Default": L.icon({
